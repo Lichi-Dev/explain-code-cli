2bb13d2c502bccc67c36865c340055db787d0318
Bonny Boben <bonnybobencr7@gmail.com>
2023-12-12 18:59:31 +0530

    first commit

---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const axios = require("axios");
+const { program } = require("commander");
+const chalk = require("chalk");
+const fs = require("fs").promises;
+const boxen = require("boxen");
+const explainPara = chalk.hex("#FFA500");
+const heading = chalk.hex("#4AF626");
+const errorColor = chalk.hex("#ff3333");
+require("dotenv").config();
+program
+  .version("1.0.0")
+  .description("CLI App to explain code using OpenAI API")
+  .option("--file <fileName>", "Explain the code from the file")
+  .name("explain")
+  .usage("--file <fileName>")
+  .action(async (options) => {
+    try {
+      const codeFilePath = options.file || "index.js";
+      const code = await fs.readFile(codeFilePath, "utf-8");
+      const explanation = await getCodeExplanation(code);
+      console.log(heading("Explanation"));
+      console.log(explainPara(boxen(explanation, { padding: 1 })));
+    } catch (error) {
+      console.error(
+        errorColor("Error reading or explaining the code:", error.message)
+      );
+    }
+  });
+
+program.parse(process.argv);
+
+async function getCodeExplanation(code) {
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        prompt: `Explain the following code in points:\n${code}`,
+        model: "gpt-3.5-turbo-instruct",
+        max_tokens: 1000,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPEN_AI_API_KEY}`,
+        },
+      }
+    );
+    return response.data.choices[0].text.trim();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to get code explanation from OpenAI API");
+  }
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Example - Explain Code CLI
+
+A CLI implementation to get explaination of a code.
+
+## Prerequisites
+
+1. [Node.js](https://nodejs.org) and [npm](https://npmjs.com) installed.
+
+## Installation
+
+1. Clone or download this repository.
+2. Create a openai API KEY [API KEY](https://platform.openai.com/api-keys). Copy or store this token in a secure place.
+3. Create an environment variable named `OPEN_AI_API_KEY`, and set its value to your API KEY obtained in step 2.
+4. Install the CLI: `npm install -g` (alternatively, you may invoke the CLI directly via `node index.js <commands>`)
+
+## Usage
+
+At any time, you can view usage instructions by entering `explain --help`.
+
+### Explain the Code
+
+To explain the code:
+
+`explain --file <fileName>`
